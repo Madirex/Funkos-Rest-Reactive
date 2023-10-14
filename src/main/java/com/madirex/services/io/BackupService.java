a2842ed07576f484fec64779c56cd01ea9cf2add
Madirex <madirexland@gmail.com>
2023-10-14 15:43:41 +0200

    BackupService ASYNC TO REACTIVE

--- src/main/java/com/madirex/services/io/BackupService.java
+++ src/main/java/com/madirex/services/io/BackupService.java
@@ -5,20 +5,20 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.madirex.exceptions.DirectoryException;
 import com.madirex.exceptions.ImportDataException;
-import com.madirex.models.Funko;
+import com.madirex.models.funko.Funko;
 import com.madirex.utils.LocalDateAdapter;
 import com.madirex.utils.LocalDateTimeAdapter;
 import com.madirex.utils.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.io.File;
 import java.lang.reflect.Type;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionException;
 
 /**
  * Clase BackupService
@@ -53,8 +53,8 @@ public class BackupService<T> {
      * @param fileName Nombre del archivo del backup
      * @param data     Datos a guardar
      */
-    public CompletableFuture<Void> exportData(String path, String fileName, T data) {
-        CompletableFuture.runAsync(() -> {
+    public Mono<Void> exportData(String path, String fileName, T data) {
+        return Mono.defer(() -> {
             File dataDir = new File(path);
             if (dataDir.exists()) {
                 String dest = path + File.separator + fileName;
@@ -66,11 +66,11 @@ public class BackupService<T> {
                 String json = gson.toJson(data);
                 Utils.getInstance().writeString(dest, json);
                 logger.debug("Backup realizado con éxito");
+                return Mono.empty();
             } else {
-                throw new CompletionException(new DirectoryException("No se creará el backup."));
+                return Mono.error(new DirectoryException("No se creará el backup."));
             }
-        }).join();
-        return CompletableFuture.completedFuture(null);
+        });
     }
 
     /**
@@ -78,25 +78,26 @@ public class BackupService<T> {
      *
      * @param path     Ruta del archivo JSON
      * @param fileName Nombre del archivo JSON
-     * @return CompletableFuture de los datos importados
+     * @return Datos importados
      */
-    public CompletableFuture<List<Funko>> importData(String path, String fileName) {
-        return CompletableFuture.supplyAsync(() -> {
-            File folder = new File(path + File.separator);
-            if (!folder.exists()) {
-                throw new CompletionException(new DirectoryException("No se creará el backup."));
-            }
-            File dataFile = new File(path + File.separator + fileName);
-            String json = new String(Utils.getInstance().getFileBytes(dataFile));
-            Type listType = new TypeToken<List<Funko>>() {
-            }.getType();
-            Gson gson = new GsonBuilder()
-                    .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
-                    .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
-                    .create();
-            return (List<Funko>) gson.fromJson(json, listType);
-        }).exceptionally(ex -> {
-            throw new CompletionException(new ImportDataException(ex.getMessage()));
-        });
+    public Flux<Funko> importData(String path, String fileName) {
+        return Mono.fromCallable(() -> {
+                    File folder = new File(path + File.separator);
+                    if (!folder.exists()) {
+                        throw new DirectoryException("No se creará el backup.");
+                    }
+                    File dataFile = new File(path + File.separator + fileName);
+                    String json = new String(Utils.getInstance().getFileBytes(dataFile));
+                    Type listType = new TypeToken<List<Funko>>() {
+                    }.getType();
+                    Gson gson = new GsonBuilder()
+                            .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
+                            .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+                            .create();
+                    List<Funko> funkoList = gson.fromJson(json, listType);
+                    return funkoList;
+                })
+                .flatMapMany(Flux::fromIterable)
+                .onErrorMap(ex -> new ImportDataException(ex.getMessage()));
     }
 }
